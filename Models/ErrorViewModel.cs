17bce4cc297f2114ec5eb694b07e1d64b349f4b7
artPerozin <perozin.arthur@gmail.com>
2026-02-01 14:44:20 -0300

    adiciona template mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ASPnetAPIrest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
